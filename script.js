cbbedfbcd2af0f51e85315179ccb996cfa57f3e2
Hao <haoyu_cn@hotmail.com>
2018-02-14 00:06:18 +0000

    basic script for HTML and JS

--- script.js
+++ script.js
@@ -1,59 +1,69 @@
 function showTabInfo(tabName) {
     var i;
-    var x = document.getElementsByClassName("content");
+    var x = document.getElementsByClassName("canvas");
     for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
+       x[i].style.display = "none";
     }
-    document.getElementById(tabName).style.display = "block";  
+    document.getElementById(tabName).style.display = "block";
 }
 
 $(document).ready(function() {
-    $("#weatherSubmit").click(function(e) {
+    $("#Button-Random").click(function(e) {
         e.preventDefault();
-        var value = $("#weatherInput").val();
-        var myurl= "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&appid=fcee3bd35c8aa28397d5fe3b55ed3e76";
+
+        console.log("button get click");
+
+        var value = Math.floor((Math.random() * 614) + 1);
+        var myurl= "http://xkcd.com/" + value +"/info.0.json" ;
+        //console.log(myurl);
+        // $.ajax({
+        //     url : myurl,
+        //     dataType : "json",
+        //     success : function(json) {
+        //
+        //         // var results = "";
+        //         // results += "<h2>" + jsonp.safe_title + "</h2>";
+        //         // results += "<img src="+ jsonp.img +"></img>";
+        //         // results += "<p>" + jsonp.alt + "</p>";
+        //         // results += "<h3>" + jsonp.year + "</h3>";
+        //         $("#xkcdResults").html(results);
+        //     }
+        // });
+
+        // $.getJSON(myurl, (json) => {
+        //   console.log(json);
+        //   var results = "";
+        //   results += "<h2>" + json.safe_title + "</h2>";
+        //   results += "<img src="+ json.img +"></img>";
+        //   results += "<p>" + json.alt + "</p>";
+        //   results += "<h3>" + json.year + "</h3>";
+        //   $("#xkcdResults").html(results);
+        // });
+
+        function getComic(json){
+          console.log(json);
+        }
+
+        $.ajax({
+          url : myurl,
+          dataType : "jsonp",
+          jsonpCallback: "getComic"
+        });
+    });
+
+    $("#chuck-Random").click(function(e) {
+        e.preventDefault();
+        var myurl= "https://api.chucknorris.io/jokes/random";
         $.ajax({
             url : myurl,
             dataType : "json",
             success : function(json) {
                 console.log(JSON.stringify(json));
                 var results = "";
-                results += '<h2>Weather in ' + json.name + "</h2>";
-                for (var i=0; i<json.weather.length; i++) {
-                    results += '<p><img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/><p>';
-                }
-                results += '<h2>' + json.main.temp + " &deg;F</h2>"
-                results += "<p>"
-                for (var i=0; i<json.weather.length; i++) {
-                    results += json.weather[i].description
-                    if (i !== json.weather.length - 1)
-                    results += ", "
-                }
-                results += "</p>";
-                results += "<p>Visibility: " + json.visibility + "</p>";
-                results += "<p>Expected High: " + json.main.temp_max + " &deg;F</p>";
-                results += "<p>Expected Low: " + json.main.temp_min + " &deg;F</p>";
-                results += "<p>Wind Speed: " + json.wind.speed + " mph</p>";
-                $("#weatherResults").html(results);
+                results += "<p>" + json.value + "</p>";
+                $("#cnResults").html(results);
             }
         });
     });
 
-    $("#questionSubmit").click(function(e) {
-        e.preventDefault();
-        var value = $("#questionInput").val();
-        var myurl= "https://api.stackexchange.com/2.2/search?order=desc&sort=activity&intitle="+ value + "&site=stackoverflow";
-        $.ajax({
-            url : myurl,
-            dataType : "json",
-            success : function(json) {
-                var results = "";
-                for (var i=0; i<json.items.length; i++) {
-                    results += "<p><a href=\"" + json.items[i].link + "\">Question: " + json.items[i].title + "</a></p>";
-                }
-                $("#questionResults").html(results);
-            }
-        });
-    });    
 });
-
