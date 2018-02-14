2d48db8e9b2f578628291a0af9bce41179e8e66b
Hao <haoyu_cn@hotmail.com>
2018-02-14 01:36:38 +0000

    need to change the mobile view

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
-function showTabInfo(tabName) {
-    var i;
-    var x = document.getElementsByClassName("canvas");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
-    }
-    document.getElementById(tabName).style.display = "block";
-}
-
 $(document).ready(function() {
+  $("#cn-button").click(function(e) {
+    $("#cnResults").html("");
+      e.preventDefault();
+      var myurl= "https://api.chucknorris.io/jokes/random";
+      $.ajax({
+          url : myurl,
+          dataType : "json",
+          success : function(json) {
+              console.log(JSON.stringify(json));
+              var results = "";
+              results += "<p>" + json.value + "</p>";
+              $("#cnResults").html(results);
+          }
+      });
+      $("#cn-canvas").show();
+      $("#xkcd-canvas").hide();
+
+  });
+
+
+
+  $("#xkcd-button").click(function(e) {
+      e.preventDefault();
+
+      console.log("button get click");
+
+      var myurl= "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke";
+      $.ajax({
+          url : myurl,
+          dataType : "json",
+          success : function(json) {
+              var results = "";
+              results += "<h2>" + json.setup + "</h2>";
+              results += "<p id=\"Result\" style=\"display:none\">" + json.punchline + "</p>";
+              $("#xkcdResults").html(results);
+          }
+      });
+      $("#xkcd-canvas").show();
+      $("#cn-canvas").hide();
+  });
+
+
+
     $("#Button-Random").click(function(e) {
         e.preventDefault();
 
